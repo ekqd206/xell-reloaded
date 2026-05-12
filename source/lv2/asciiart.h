af91c61aa0035bfe3cf2ca24cea34bd309b86356
ekqd206 <gawelkacper2011@gmail.com>
2026-05-13 00:24:54 +0200

    Change asciiart and asciitail to const char*

--- source/lv2/asciiart.h
+++ source/lv2/asciiart.h
@@ -1,15 +1,16 @@
-static char* asciiart = "\n"
-	"        _             _     ____   ___   __\n"
-	"    ___| | ____ _  __| |   |___ \ / _ \ / /_\n"
-	"   / _ \ |/ / _` |/ _` |     __) | | | | '_ \\n"
-	"  |  __/   < (_| | (_| |    / __/| |_| | (_) |\n"
-	"   \___|_|\_\__, |\__,_|___|_____|\___/ \___/\n"
-	"               |_|    |_____|\n\n";
-static char* asciitail =
-	"              Powered by ekqd_206 & KARCAINE's Custom XeLL - Xenon Linux Loader, got RELOADED :D\n";
-    "              Special Corona & Winchester Compatible XeLL version\n";
-    " Copyright (C) 2001-2010 Microsoft Corporation. All rights reserved.\n";
-    "               Make it faster by making it sleep...\n";
-    "               Make it faster by making it consume more power...\n";
-    "               Make it faster by awaking the other cores...\n";
-    "               Fine, they all came back...\n\n";
+static const char* asciiart = "\n"
+"        _             _     ____   ___   __\n"
+"    ___| | ____ _  __| |   |___ \\ / _ \\ / /_\n"
+"   / _ \\ |/ / _` |/ _` |     __) | | | | '_ \\\n"
+"  |  __/   < (_| | (_| |    / __/| |_| | (_) |\n"
+"   \\___|_|\\_\\__, |\\__,_|___|_____|\\___/ \\___/\n"
+"               |_|    |_____|\n\n";
+
+static const char* asciitail =
+"              Powered by ekqd_206 & KARCAINE's Custom XeLL - Xenon Linux Loader, got RELOADED :D\n"
+"              Special Corona & Winchester Compatible XeLL version\n"
+" Copyright (C) 2001-2010 Microsoft Corporation. All rights reserved.\n"
+"               Make it faster by making it sleep...\n"
+"               Make it faster by making it consume more power...\n"
+"               Make it faster by awaking the other cores...\n"
+"               Fine, they all came back...\n\n";
